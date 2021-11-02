ab095a833accf17ae08e2ad257bc49a75bd6e06c
David Broida <davidcbroida@gmail.com>
2021-11-02 11:42:02 +0800

    first commit

---
+++ hack-or-snooze-ajax-api/js/nav.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+	console.debug('navAllStories', evt);
+	hidePageComponents();
+	// putUserStoriesOnPage();
+	putStoriesOnPage();
+}
+
+$body.on('click', '#nav-all', navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+	console.debug('navLoginClick', evt);
+	hidePageComponents();
+	$loginForm.show();
+	$signupForm.show();
+}
+
+$navLogin.on('click', navLoginClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+	console.debug('updateNavOnLogin');
+	$('.main-nav-links').show();
+	$navLogin.hide();
+	$navLogOut.show();
+	$navUserProfile.text(`${currentUser.username}`).show();
+}
+
+function navSubmitStory(evt) {
+	console.debug('navSubmitStory', evt);
+	$submitForm.show();
+	hidePageComponents();
+	$allStoriesList.show();
+}
+$navSubmitStory.on('click', navSubmitStory);
+
+function navFavoriteStories(evt) {
+	console.debug('navFavoriteStories', evt);
+	$ownStories.show();
+	$favoritedStories.show();
+	hidePageComponents();
+	addFavoriteStories();
+}
+$navFavoriteStories.on('click', navFavoriteStories);
+
+function navMyStories(evt) {
+	console.debug('navMyStories', evt);
+	hidePageComponents();
+	putUserStoriesOnPage();
+	$ownStories.show();
+}
+$navMyStories.on('click', navMyStories);
+
+// navProfileClick;
